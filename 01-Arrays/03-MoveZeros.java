22f7c1e2f4c8da2ac0a6e88fbc7db3b53d27e009
imakhlaq <imakhlaqXD@gmail.com>
2023-06-04 14:48:44 +0530

    move zeros

---
+++ 01-Arrays/03-MoveZeros.java
@@ -0,0 +1,34 @@
+class MoveZeros {
+
+    static void moveZeros(int nums[]) {
+
+        int location = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] != 0) {
+                nums[location] = nums[i];
+                location++;
+
+            }
+        }
+
+        while (location < nums.length) {
+            nums[location++] = 0;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = { 0, 0, 1, 0, 3, 12 };
+
+        moveZeros(arr);
+
+        for (int x : arr) {
+            System.out.println(x);
+        }
+
+    }
+
+}
