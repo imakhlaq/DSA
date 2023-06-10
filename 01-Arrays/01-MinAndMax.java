d2a6043d4505b91c36e2fb3988965d075a4f48cb
imakhlaq <imakhlaqXD@gmail.com>
2023-06-10 23:48:34 +0530

    reverse

--- 01-Arrays/01-MinAndMax.java
+++ 01-Arrays/01-MinAndMax.java
@@ -3,8 +3,27 @@
  */
 class MinAndMax {
 
+    public static void minAndMax(int arr[]) {
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int a : arr) {
+            if (a < min) {
+                min = a;
+            }
+            if (a > max) {
+                max = a;
+            }
+        }
+        System.out.print(min);
+        System.out.println(max);
+    }
+
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        int arr[] = { 2, 4, -3, 1, 3, -2, 1, -5, 7, 2 };
+        minAndMax(arr);
 
     }
 
--- 01-Arrays/02-Reverse.java
+++ 01-Arrays/02-Reverse.java
@@ -1,6 +1,30 @@
 class Reverse {
 
+    static void reverseArray(int arr[]) {
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+
+        }
+
+    }
+
     public static void main(String[] args) {
-        System.out.println();
+        int arr[] = { 3, 1, 2, 2, 0, 1, 0, 3, 12 };
+
+        reverseArray(arr);
+
+        for (int x : arr) {
+            System.out.println(x);
+        }
+
     }
 }
